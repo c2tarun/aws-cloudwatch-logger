43cfb1f2e99b504312d186e2ae117aeb62333211
Tarun K. Mall <c2tarun@gmail.com>
2020-09-05 17:45:21 -0700

    Adding support for session token.

--- index.js
+++ index.js
@@ -7,72 +7,72 @@
 */
 
 const axios = require('axios')
-const aws4  = require('aws4')
+const aws4 = require('aws4')
 
-const getRequestParams = (method, region, payload, keys={}) => {
-	if (!region)
-		throw new Error('Missing required argument: \'region\' is required.')
-	if (!method)
-		throw new Error('Missing required argument: \'method\' is required.')
+const getRequestParams = (method, region, payload, keys = {}) => {
+  if (!region)
+    throw new Error('Missing required argument: \'region\' is required.')
+  if (!method)
+    throw new Error('Missing required argument: \'method\' is required.')
 
-	let opts = {
-		service: 'logs', 
-		region: region, 
-		path: `/?Action=${method}`, 
-		headers: { 
-			'X-Amz-Target': `Logs_20140328.${method}`,
-			'Accept': 'application/json',
-			'Content-Type': 'application/x-amz-json-1.1'
-		}
-	}
+  let opts = {
+    service: 'logs',
+    region: region,
+    path: `/?Action=${method}`,
+    headers: {
+      'X-Amz-Target': `Logs_20140328.${method}`,
+      'Accept': 'application/json',
+      'Content-Type': 'application/x-amz-json-1.1'
+    }
+  }
 
-	if (payload)
-		opts.body = JSON.stringify(payload)
+  if (payload)
+    opts.body = JSON.stringify(payload)
 
-	if (keys.accessKeyId && keys.secretAccessKey)
-		aws4.sign(opts, { accessKeyId: keys.accessKeyId, secretAccessKey: keys.secretAccessKey })
-	else
-		aws4.sign(opts)
+  if (keys.accessKeyId && keys.secretAccessKey)
+    aws4.sign(opts, keys)
+  else
+    aws4.sign(opts)
 
-	return {
-		uri: `https://${opts.hostname}${opts.path}`,
-		headers: opts.headers
-	}
+  return {
+    uri: `https://${opts.hostname}${opts.path}`,
+    headers: opts.headers
+  }
 }
 
-const createLogStream = (logStreamName, { logGroupName, region, accessKeyId, secretAccessKey, local }) => {
-	if (local && local != 'false')
-		return Promise.resolve({ message: 'No need to create stream. Local mode is on.' })
+const createLogStream = (logStreamName, {logGroupName, region, accessKeyId, secretAccessKey, local}) => {
+  if (local && local != 'false')
+    return Promise.resolve({message: 'No need to create stream. Local mode is on.'})
 
-	if (!logGroupName)
-		throw new Error('Missing required argument: \'logGroupName\' is required.')
-	if (!logStreamName)
-		throw new Error('Missing required argument: \'logStreamName\' is required.')
-	if (!secretAccessKey)
-		throw new Error('Missing required argument: \'secretAccessKey\' is required.')
-	if (!accessKeyId)
-		throw new Error('Missing required argument: \'accessKeyId\' is required.')
-	if (!region)
-		throw new Error('Missing required argument: \'region\' is required.')
+  if (!logGroupName)
+    throw new Error('Missing required argument: \'logGroupName\' is required.')
+  if (!logStreamName)
+    throw new Error('Missing required argument: \'logStreamName\' is required.')
+  if (!secretAccessKey)
+    throw new Error('Missing required argument: \'secretAccessKey\' is required.')
+  if (!accessKeyId)
+    throw new Error('Missing required argument: \'accessKeyId\' is required.')
+  if (!region)
+    throw new Error('Missing required argument: \'region\' is required.')
 
-	const service = 'CreateLogStream'
-	const payload = {
-		logGroupName: logGroupName,
-		logStreamName: logStreamName
-	}
+  const service = 'CreateLogStream'
+  const payload = {
+    logGroupName: logGroupName,
+    logStreamName: logStreamName
+  }
 
-	const { uri, headers } = getRequestParams(service, region, payload, { accessKeyId, secretAccessKey })
+  const {uri, headers} = getRequestParams(service, region, payload, {accessKeyId, secretAccessKey})
 
-	const request = axios.create({
-		baseURL: uri,
-		headers: headers
-	})
+  const request = axios.create({
+    baseURL: uri,
+    headers: headers
+  })
 
-	return request.post('', payload)
-		.then(results => results.data)
-		.catch(err => {
-			throw new Error(err.response.data.message)
-		})
+  return request.post('', payload)
+    .then(results => results.data)
+    .catch(err => {
+      throw new Error(err.response.data.message)
+    })
 }
 
 const _sequenceTokens = new Map()
@@ -91,152 +91,155 @@ const _sequenceTokens = new Map()
  * @param  {Number} 				retryCount    	[description]
  * @return {Promise}               					[description]
  */
-const addLogsToStream = (entry, logGroupName, logStreamName, region, keys={}, sequenceToken='', retryCount=0) => {
-	if (!logGroupName)
-		throw new Error('Missing required argument: \'logGroupName\' is required.')
-	if (!logStreamName)
-		throw new Error('Missing required argument: \'logStreamName\' is required.')
-	if (!entry)
-		throw new Error('Missing required argument: \'entry\' is required.')
-	
-	const entryType = typeof(entry)
-	const now = Date.now()
+const addLogsToStream = (entry, logGroupName, logStreamName, region, keys = {}, sequenceToken = '', retryCount = 0) => {
+  if (!logGroupName)
+    throw new Error('Missing required argument: \'logGroupName\' is required.')
+  if (!logStreamName)
+    throw new Error('Missing required argument: \'logStreamName\' is required.')
+  if (!entry)
+    throw new Error('Missing required argument: \'entry\' is required.')
 
-	const events = 
-		// entry is a message
-		entryType == 'string' ? [{ message: entry, timestamp: now }] :
-		// entry is a well-formatted log event
-			entryType == 'object' && entry.timestamp ? [{ message: entry.message, timestamp: entry.timestamp }] :
-				// entry is an array of items
-				entry.length > 0 ? entry.map(e => 
-					typeof(e) == 'string' ? { message: e, timestamp: now } :
-						e.timestamp ? { message: e.message, timestamp: e.timestamp } : null).filter(x => x)
-					: null
+  const entryType = typeof (entry)
+  const now = Date.now()
 
-	const nothingToLog = events == null
+  const events =
+    // entry is a message
+    entryType == 'string' ? [{message: entry, timestamp: now}] :
+      // entry is a well-formatted log event
+      entryType == 'object' && entry.timestamp ? [{message: entry.message, timestamp: entry.timestamp}] :
+        // entry is an array of items
+        entry.length > 0 ? entry.map(e =>
+          typeof (e) == 'string' ? {message: e, timestamp: now} :
+            e.timestamp ? {message: e.message, timestamp: e.timestamp} : null).filter(x => x)
+          : null
 
-	const service = 'PutLogEvents'
-	let payload = {
-		logEvents: events,
-		logGroupName: logGroupName,
-		logStreamName: logStreamName
-	}
+  const nothingToLog = events == null
 
-	const tokenKey = logGroupName + '__' + logStreamName
-	sequenceToken = sequenceToken || _sequenceTokens.get(tokenKey)
-	if (sequenceToken)
-		payload.sequenceToken = sequenceToken
+  const service = 'PutLogEvents'
+  let payload = {
+    logEvents: events,
+    logGroupName: logGroupName,
+    logStreamName: logStreamName
+  }
 
-	const { uri, headers } = getRequestParams(service, region, payload, keys)
+  const tokenKey = logGroupName + '__' + logStreamName
+  sequenceToken = sequenceToken || _sequenceTokens.get(tokenKey)
+  if (sequenceToken)
+    payload.sequenceToken = sequenceToken
 
-	const request = axios.create({
-		baseURL: uri,
-		headers: headers
-	})
+  const {uri, headers} = getRequestParams(service, region, payload, keys)
 
-	return retryCount > 3 || nothingToLog ? Promise.resolve(null) : request.post('', payload)
-		.then(results => {
-			//console.log('Yes')
-			const token = results.data.nextSequenceToken
-			_sequenceTokens.set(tokenKey, token)
-		})
-		.catch(err => {
-			//console.log('Oops')
-			const token = err.response.data.expectedSequenceToken
-			if (token) {
-				_sequenceTokens.set(tokenKey, token)
-				retryCount += 1
-				return addLogsToStream(entry, logGroupName, logStreamName, region, keys, token, retryCount)
-			}
-			else 
-				console.error(err.response.data)
-		})
+  const request = axios.create({
+    baseURL: uri,
+    headers: headers
+  })
+
+  return retryCount > 3 || nothingToLog ? Promise.resolve(null) : request.post('', payload)
+    .then(results => {
+      //console.log('Yes')
+      const token = results.data.nextSequenceToken
+      _sequenceTokens.set(tokenKey, token)
+    })
+    .catch(err => {
+      //console.log('Oops')
+      const token = err.response.data.expectedSequenceToken
+      if (token) {
+        _sequenceTokens.set(tokenKey, token)
+        retryCount += 1
+        return addLogsToStream(entry, logGroupName, logStreamName, region, keys, token, retryCount)
+      }
+      else
+        console.error(err.response.data)
+    })
 }
 
-const delayFn = (fn,time) => makeQuerablePromise((new Promise((onSuccess) => setTimeout(() => onSuccess(), time))).then(() => fn()))
+const delayFn = (fn, time) => makeQuerablePromise((new Promise((onSuccess) => setTimeout(() => onSuccess(), time))).then(() => fn()))
 const makeQuerablePromise = promise => {
-	// Don't modify any promise that has been already modified.
-	if (promise.isResolved) return promise
+  // Don't modify any promise that has been already modified.
+  if (promise.isResolved) return promise
 
-	// Set initial state
-	let isPending = true
-	let isRejected = false
-	let isFulfilled = false
+  // Set initial state
+  let isPending = true
+  let isRejected = false
+  let isFulfilled = false
 
-	// Observe the promise, saving the fulfillment in a closure scope.
-	let result = promise.then(
-		v => {
-			isFulfilled = true
-			isPending = false
-			return v
-		}, 
-		e => {
-			isRejected = true
-			isPending = false
-			throw e
-		}
-	)
+  // Observe the promise, saving the fulfillment in a closure scope.
+  let result = promise.then(
+    v => {
+      isFulfilled = true
+      isPending = false
+      return v
+    },
+    e => {
+      isRejected = true
+      isPending = false
+      throw e
+    }
+  )
 
-	result.isFulfilled = () => isFulfilled
-	result.isPending = () => isPending
-	result.isRejected = () => isRejected
-	return result
+  result.isFulfilled = () => isFulfilled
+  result.isPending = () => isPending
+  result.isRejected = () => isRejected
+  return result
 }
 
 const _logbuffer = new WeakMap()
 const Logger = class {
-	constructor({ logGroupName, logStreamName, region, accessKeyId, secretAccessKey, uploadFreq, local }) {
-		if (!logGroupName)
-			throw new Error('Missing required argument: \'logGroupName\' is required.')
-		if (!logStreamName)
-			throw new Error('Missing required argument: \'logStreamName\' is required.')
-		if (!region)
-			throw new Error('Missing required argument: \'region\' is required.')
-		if (!secretAccessKey)
-			throw new Error('Missing required argument: \'secretAccessKey\' is required.')
-		if (!accessKeyId)
-			throw new Error('Missing required argument: \'accessKeyId\' is required.')
+  constructor({logGroupName, logStreamName, region, accessKeyId, secretAccessKey, sessionToken = null, uploadFreq, local}) {
+    if (!logGroupName)
+      throw new Error('Missing required argument: \'logGroupName\' is required.')
+    if (!logStreamName)
+      throw new Error('Missing required argument: \'logStreamName\' is required.')
+    if (!region)
+      throw new Error('Missing required argument: \'region\' is required.')
+    if (!secretAccessKey)
+      throw new Error('Missing required argument: \'secretAccessKey\' is required.')
+    if (!accessKeyId)
+      throw new Error('Missing required argument: \'accessKeyId\' is required.')
 
-		const keys = accessKeyId && secretAccessKey ? { accessKeyId, secretAccessKey } : {}
+    const keys = accessKeyId && secretAccessKey ? {accessKeyId, secretAccessKey} : {}
+    if (sessionToken) {
+      keys.sessionToken = sessionToken;
+    }
 
-		let log
-		if (!uploadFreq || uploadFreq < 0) {
-			log = (...args) => {
-				const logs = (args || []).map(x => JSON.stringify(x))
-				// console.log('Logging now...')
-				// console.log(logs)
-				addLogsToStream(logs, logGroupName, logStreamName, region, keys)
-			}
-		}
-		else {
-			log = (...args) => {
-				// 1. Accumulate all logs
-				const now = Date.now()
-				const logs = (args || []).map(x => ({ message: JSON.stringify(x), timestamp: now }))
-				let latestBuffer = (_logbuffer.get(this) || { latest: now, data: [], job: null })
-				latestBuffer.data = latestBuffer.data.concat(logs)
+    let log
+    if (!uploadFreq || uploadFreq < 0) {
+      log = (...args) => {
+        const logs = (args || []).map(x => JSON.stringify(x))
+        // console.log('Logging now...')
+        // console.log(logs)
+        addLogsToStream(logs, logGroupName, logStreamName, region, keys)
+      }
+    }
+    else {
+      log = (...args) => {
+        // 1. Accumulate all logs
+        const now = Date.now()
+        const logs = (args || []).map(x => ({message: JSON.stringify(x), timestamp: now}))
+        let latestBuffer = (_logbuffer.get(this) || {latest: now, data: [], job: null})
+        latestBuffer.data = latestBuffer.data.concat(logs)
 
-				// 2. If no job has ever been started, start it, or if the job is ready to process more
-				if (!latestBuffer.job || !latestBuffer.job.isPending()) {
-					latestBuffer.job = makeQuerablePromise(delayFn(() => {
-						const { latest, data, job } = (_logbuffer.get(this) || { latest: now, data: [], job: null })
-						// console.log('Finally logging now...')
-						// console.log(data)
-						_logbuffer.set(this, { latest, data:[], job })
-						addLogsToStream(data, logGroupName, logStreamName, region, keys)
-					}, uploadFreq))
-				}
-				//console.log('Buffering logs now...')
-				// 3. In any case, memoize 
-				_logbuffer.set(this, latestBuffer)
-			}
-		}
+        // 2. If no job has ever been started, start it, or if the job is ready to process more
+        if (!latestBuffer.job || !latestBuffer.job.isPending()) {
+          latestBuffer.job = makeQuerablePromise(delayFn(() => {
+            const {latest, data, job} = (_logbuffer.get(this) || {latest: now, data: [], job: null})
+            // console.log('Finally logging now...')
+            // console.log(data)
+            _logbuffer.set(this, {latest, data: [], job})
+            addLogsToStream(data, logGroupName, logStreamName, region, keys)
+          }, uploadFreq))
+        }
+        //console.log('Buffering logs now...')
+        // 3. In any case, memoize 
+        _logbuffer.set(this, latestBuffer)
+      }
+    }
 
-		this.log = local && local != 'false' ? console.log : log
-	}
+    this.log = local && local != 'false' ? console.log : log
+  }
 }
 
 module.exports = {
-	createLogStream,
-	Logger
+  createLogStream,
+  Logger
 }
